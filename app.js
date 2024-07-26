5cd3640249d6ea8c89dcc774ed65cef6c4daf2ed
LucasMeloGomes <160232812+LucasMeloGomes@users.noreply.github.com>
2024-07-26 12:07:32 -0300

    c

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = ok;
+alert('que tal participa de uma brincadeira nada divertida');
+let numeroSecreto = 666;
 console.log('numeroSecreto')
 let chute = prompt('Escolha um número entre 1 e 10');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (5)');
+alert('parábens vc é um lixo que está perdendo tempo com isso');
 } else {
-alert('Você errou :(')
+alert('Você errou seu lixo imundo, pq vc ainda está vivo desperdiçando oxigenio')
 }
